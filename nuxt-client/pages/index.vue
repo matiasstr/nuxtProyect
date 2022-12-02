2d82891ecd9040fb2198e6938d46aa88170992f9
matiasstr <matias.strficek@gmail.com>
2022-12-02 11:07:38 -0300

    Terminado grid de componentes

--- nuxt-client/pages/index.vue
+++ nuxt-client/pages/index.vue
@@ -1,11 +1,28 @@
 <template>
   <v-app>
 
-    <AxiosData />
-  
-  
-    <DatosStore />
-    <v-btn class="mt-10" to="/formulario">A FORMULARIO</v-btn>
+    <v-container>
+      <v-row>
+
+        <v-col cols="12" md="3">
+
+          <DatosStore />
+        </v-col>
+
+        <v-spacer></v-spacer>
+        <v-col cols="12" md="9">
+
+          <AxiosData />
+        </v-col >
+      </v-row>
+
+      <v-container>
+        <v-row justify="center">
+          <v-btn class="mt-10" to="/formulario" color="indigo">A FORMULARIO</v-btn>
+        </v-row>
+      </v-container>
+
+    </v-container>
   </v-app>
 
 
@@ -15,13 +32,13 @@
 import DatosStore from "@/components/DatosStore.vue"
 import AxiosData from "@/components/AxiosData.vue"
 export default {
-  components:{
+  components: {
     DatosStore,
     AxiosData
   },
   data() {
     return {
-      userArr : this.$store.state.store.user,
+      userArr: this.$store.state.store.user,
     }
   },
   methods: {
